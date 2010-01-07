e3ddd46e0878a080d2b8d7c919bb57be487408a2
Przemyslaw Kowalczyk <szeryf@negativeiq.pl>
2010-01-07 11:03:11 +0100

    ensure that examples use ../lib/cromwell.rb not the installed gem

--- examples/example1.rb
+++ examples/example1.rb
@@ -1,4 +1,6 @@
-require 'rubygems'
+# ensure that we use ../lib/cromwell.rb, not the installed gem
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'cromwell'
 
 puts 'See you in a while...'
